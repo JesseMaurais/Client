4321ae95a510db4f9ee06436d9ae35450c9d12e1
Jesse Maurais <jessemaurais@gmail.com>
2019-01-12 23:10:49 +0000

    Better searching for span_view implementation

--- dbg.hpp
+++ dbg.hpp
@@ -20,15 +20,15 @@ namespace debug
 }
 constexpr bool DEBUG = true;
 #define TEST(unit, ...) static struct : debug::test { void run() final { __VA_ARGS__; } using test::test; } unit(#unit)
-#define ASSERT(__VA_ARGS__) if (not(__VA_ARGS__)) throw __FILE__ ":" STRING(__LINE__) ": " #__VA_ARGS__
-#define VERIFY(__VA_ARGS__) ASSERT(__VA_ARGS__)
-#define verify(__VA_ARGS__) assert(__VA_ARGS__)
+#define ASSERT(...) if (not(__VA_ARGS__)) throw __FILE__ ":" STRING(__LINE__) ": " #__VA_ARGS__
+#define VERIFY(...) ASSERT(__VA_ARGS__)
+#define verify(...) assert(__VA_ARGS__)
 #else
 constexpr bool DEBUG = false;
 #define TEST(name, ...)
-#define ASSERT(__VA_ARGS__)
-#define VERIFY(__VA_ARGS__) (__VA_ARGS__)
-#define verify(__VA_ARGS__) (__VA_ARGS__)
+#define ASSERT(...)
+#define VERIFY(...) (__VA_ARGS__)
+#define verify(...) (__VA_ARGS__)
 #endif
 
 #define ASSERT_EQ(a, b) ASSERT((a) == (b))
--- str.hpp
+++ str.hpp
@@ -1,6 +1,9 @@
 #ifndef str_hpp
 #define str_hpp
 
+// Try to get the lightest of string containers, a span of string views.
+// Pull in string buffers and pairs as well for type completeness.
+
 #if __has_include(<string_view>)
 #include <string_view>
 namespace fmt
@@ -21,16 +24,45 @@ namespace fmt
 #error Cannot find an implementation of std::string_view.
 #endif
 
-#include <utility>
+#if __has_include(<span>)
+#include <span>
+namespace fmt
+{
+	using span_view = std::span<fmt::string_view>;
+	using wspan_view = std::span<fmt::wstring_view>;
+	template <typename... Args> using basic_span_view = std::span<basic_string_view<Args...>>;
+}
+#elif __has_include(<experimetal/span>)
+#include <experimental/span>
+namespace fmt
+{
+	using span_view = std::experimental::span<fmt::string_view>;
+	using wspan_view = std::experimental::span<fmt::wstring_view>;
+	`template <typename... Args> using basic_span_view = std::span<basic_string_view<Args...>>;
+}
+#else
+//#warning Cannot find an implementation of std::span. Using std::vector instead.
 #include <vector>
+namespace fmt
+{
+	using span_view = std::vector<fmt::string_view>;
+	using wspan_view = std::vector<fmt::wstring_view>;
+	template <typename... Args> using basic_span_view = std::vector<basic_string_view<Args...>>;
+}
+#endif
+
+#include <utility>
 #include <string>
 
 namespace fmt
 {
 	using std::string;
 	using std::wstring;
+
 	using pair = std::pair<string, string>;
-	using span_view = std::vector<string_view>;
+	using wpair = std::pair<wstring, wstring>;
+	using pair_view = std::pair<string_view, string_view>;
+	using wpair_view = std::pair<wstring_view, wstring_view>;
 }
 
 #endif // file
