9db324087f4eb610a70803d3ee4dfe1007c6ae8f
Jesse Maurais <Jesse.maurais@geoslope.com>
2019-01-16 15:16:36 -0700

    Fixed most warnings for cl -W4

--- int.hpp
+++ int.hpp
@@ -1,7 +1,6 @@
 #ifndef int_hpp
 #define int_hpp
 
-#include <cmath>
 #include <cstddef>
 #include <type_traits>
 #include "dbg.hpp"
@@ -14,7 +13,7 @@ namespace
 		using to_type = typename std::make_unsigned<T>::type;
 		static_assert(std::is_signed<T>::value);
 		static_assert(std::is_integral<T>::value);
-		assert(not std::signbit(s));
+		assert(T(0) <= s);
 		return static_cast<to_type>(s);
 	}
 
@@ -25,14 +24,22 @@ namespace
 		static_assert(std::is_unsigned<T>::value);
 		static_assert(std::is_integral<T>::value);
 		auto const s = static_cast<to_type>(u);
-		assert(not std::signbit(s));
+		assert(T(0) <= s);
 		return s;
 	}
 
 	template <typename T>
-	inline auto to_size(T const& t)
+	inline std::size_t to_size(T const& t)
 	{
-		return static_cast<std::size_t>(t);
+		if constexpr (std::is_signed<T>::value)
+		{
+			auto const u = to_unsigned(t);
+			return static_cast<std::size_t>(u);
+		}
+		else
+		{
+			return static_cast<std::size_t>(t);
+		}
 	}
 }
 
