cc1bfc6ddcaa7d168d0d8aaf4e56bbcead892ee7
Jesse Maurais <jessemaurais@gmail.com>
2019-07-08 16:04:14 -0600

    Split large header

--- fmt.hpp
+++ fmt.hpp
@@ -1,202 +1,17 @@
 #ifndef fmt_hpp
 #define fmt_hpp
 
-#include <functional>
 #include <algorithm>
+#include <functional>
 #include <cinttypes>
 #include <cstdlib>
 #include <locale>
 #include "str.hpp"
 #include "its.hpp"
+#include "tos.hpp"
 
 namespace fmt
 {
-	//
-	// Generic string converters
-	//
-
-	template <typename T>
-	inline string to_string(T const& x)
-	{
-		return std::to_string(x);
-	}
-
-	template <typename T>
-	inline wstring to_wstring(T const& x)
-	{
-		return std::to_wstring(x);
-	}
-
-	template <>
-	inline string to_string(string const& s)
-	{
-		return s;
-	}
-
-	template <>
-	inline wstring to_wstring(wstring const& w)
-	{
-		return w;
-	}
-
-	template <>
-	inline string to_string(string_view const& s)
-	{
-		return string(s.data(), s.size());
-	}
-
-	template <>
-	inline wstring to_wstring(wstring_view const& w)
-	{
-		return wstring(w.data(), w.size());
-	}
-
-	template <>
-	inline string to_string(char const*const& s)
-	{
-		return s ? string(s) : string();
-	}
-
-	template <>
-	inline wstring to_wstring(wchar_t const*const& w)
-	{
-		return w ? wstring(w) : wstring();
-	}
-
-	template <>
-	inline string to_string(char *const& s)
-	{
-		return s ? string(s) : string();
-	}
-
-	template <>
-	inline wstring to_wstring(wchar_t *const& w)
-	{
-		return w ? wstring(w) : wstring();
-	}
-
-	template <>
-	inline string to_string(char const& c)
-	{
-		return string(1, c);
-	}
-
-	template <>
-	inline wstring to_wstring(wchar_t const& c)
-	{
-		return wstring(1, c);
-	}
-
-	// Wide & narrow conversions
-
-	template <>
-	inline string to_string(wstring_view const& w)
-	{
-		string s;
-		for (auto const c : narrow(w)) s.push_back(c);
-		return s;
-	}
-
-	template <>
-	inline wstring to_wstring(string_view const& s)
-	{
-		wstring w;
-		for (auto const c : widen(s)) w.push_back(c);
-		return w;
-	}
-
-	template <>
-	inline string to_string(wstring const& w)
-	{
-		return to_string(wstring_view(w));
-	}
-
-	template <>
-	inline wstring to_wstring(string const& s)
-	{
-		return to_wstring(string_view(s));
-	}
-
-	template <>
-	inline string to_string(wchar_t const*const& w)
-	{
-		return to_string(wstring_view(w));
-	}
-
-	template <>
-	inline wstring to_wstring(char const*const& s)
-	{
-		return to_wstring(string_view(s));
-	}
-
-	template <>
-	inline string to_string(wchar_t *const& w)
-	{
-		return to_string(wstring_view(w));
-	}
-
-	template <>
-	inline wstring to_wstring(char *const& s)
-	{
-		return to_wstring(string_view(s));
-	}
-
-	template <>
-	inline string to_string(wchar_t const& c)
-	{
-		return to_string(wstring(1, c));
-	}
-
-	template <>
-	inline wstring to_wstring(char const& c)
-	{
-		return to_wstring(string(1, c));
-	}
-
-	// Basic predicates
-
-	inline bool empty(string const& s)
-	{
-		return s.empty();
-	}
-
-	inline bool empty(wstring const& s)
-	{
-		return s.empty();
-	}
-
-	inline bool empty(string_view s)
-	{
-		return s.empty();
-	}
-
-	inline bool empty(wstring_view s)
-	{
-		return s.empty();
-	}
-
-	inline bool empty(string_pair const& p)
-	{
-		return empty(p.first) and empty(p.second);
-	}
-
-	inline bool empty(string_view_pair const& p)
-	{
-		return empty(p.first) and empty(p.second);
-	}
-
-	inline bool empty(string_view_span const &v)
-	{
-		for (auto const& u : v)
-		{
-			if (not empty(u))
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-
 	//
 	// Character class injection
 	//
@@ -215,7 +30,6 @@ namespace fmt
 		using string_pair = fmt::basic_string_pair<Char>;
 		using size_pair = fmt::basic_string_size_pair<Char>;
 		using iterator = typename string_view_span::iterator;
-		using range = fmt::basic_string_view_span_range<Char>;
 		using size = typename string_view::size_type;
 		using div = std::imaxdiv_t;
 
