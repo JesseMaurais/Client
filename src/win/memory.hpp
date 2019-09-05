4dfe4bb4bb392c239ba64b309a59ab4b143d6f79
Jesse Maurais <jessemaurais@gmail.com>
2019-09-05 16:16:52 -0600

    Add unstaged/untracked

---
+++ src/win/memory.hpp
@@ -0,0 +1,31 @@
+#ifndef win_mem_hpp
+#define win_mem_hpp
+
+#include "win.hpp"
+#include "ptr.hpp"
+
+namespace sys::win
+{
+	template <typename C>
+	auto make_view(HANDLE h, DWORD dw, off_t off = 0, size_t sz = 0, C* ptr = nullptr)
+	{
+		ptr = MapViewOfFileEx(h, dw, HIWORD(off), LOWORD(off), sz, ptr);
+		if (nullptr == ptr)
+		{
+			sys::win::err(here, "MapViewOfFileEx", dw, off, sz, ptr);
+		}
+
+		return make_ptr(ptr, [](auto ptr)
+		{
+			if (nullptr != ptr)
+			{
+				if (not UnmapViewOfFile(ptr))
+				{
+					sys::win::err(here, "UnmapViewOfFile", ptr);
+				}
+			}
+		});
+	}
+}
+
+#endif
